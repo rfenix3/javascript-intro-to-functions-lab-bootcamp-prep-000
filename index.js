4c901c89810ac931d292a8121a9242003fd75a10
Richard Fenix <richard.fenix@gmail.com>
2018-02-23 06:32:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,12 +17,21 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
+<<<<<<< HEAD
   if (string === string.toLowerCase()) {
     return "I can't hear you!";
   } else if (string === string.toUpperCase()) {
     return "YES INDEED!";
   } else if (string === "I love you, Grandma.") {
     return "I love you, too."
+=======
+  if (string == string.toLowerCase()) {
+    return "I can't hear you!";
+  } else if (string == string.toUpperCase()) {
+    return "YES INDEED!";
+  } else if (string == "I love you, Grandma.") {
+    return ""
+>>>>>>> 67edde778e8076d9e06a414b7ded8d9f922156d2
   }
 }
 
